37c0e82026298a8f991847a31bf20e55a71bffa5
Sebastian <s.kloesel@gmx.de>
2022-03-15 21:59:24 +0100

    Initial Commit

---
+++ FullScreenCover_Data_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FullScreenCover_Data_SwiftUI
+//
+//  Created by Sebastian Klösel on 15.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FullScreenCover_Data_SwiftUI/FullScreenCover_Data_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullScreenCover_Data_SwiftUIApp.swift
+//  FullScreenCover_Data_SwiftUI
+//
+//  Created by Sebastian Klösel on 15.03.22.
+//
+
+import SwiftUI
+
+@main
+struct FullScreenCover_Data_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
